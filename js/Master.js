c497b05d456d35edd2f0ff3a335810218bf2d0bd
DESKTOP-J71G3OU\CAT <qwwewe56793@gmail.com>
2021-06-04 03:16:29 +0800

    電子商務前端第一版

---
+++ js/Master.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(() => {
+	$('#body').load('HomePage.html');
+});
+
+function Loader(url) {
+	$('#body').load(url);
+}
+
+function test() {
+	alert('just test!');
+}
+
+var kv = new function (_key,_val) {
+	this.Key = _key;
+	this.Val = _val;
+}
+
+$('.ui.dropdown').dropdown();
+
